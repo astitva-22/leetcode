022125bc8899bda5052ab95cf7a67fe4849c3f66
astitva-22 <astitvashukla587@gmail.com>
2026-03-17 14:39:00 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,12 +3,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum=nums[0];
         int maxSum=sum;
-        // int mini=INT_MIN;
         for(int i=1;i<nums.size();i++){
             sum=max(sum+nums[i],nums[i]);
-            
-            maxSum=max(sum,maxSum);
-            
+            maxSum=max(sum,maxSum);    
         }
         return maxSum;
     }
