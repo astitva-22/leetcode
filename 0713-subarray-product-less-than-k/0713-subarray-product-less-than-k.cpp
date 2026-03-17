9d9246634315eaf14ccba78a7b4cdedf4a3dc900
astitva-22 <astitvashukla587@gmail.com>
2026-03-17 15:13:08 +0530

    Time: 5 ms (26.5%), Space: 65.3 MB (35.13%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k<=1) return 0;
+
+        int ans=1;
+        int cnt=0;
+
+        int l=0;
+        for(int r=0;r<nums.size();r++){
+            ans *= nums[r];
+            while(l<=r && ans>=k){
+                ans /= nums[l++];
+            }
+            cnt += r-l+1;
+
+        }
+        return cnt;
+    }
+};
