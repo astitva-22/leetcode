36bae306e3b5b0d47f5a7ec145e5e2a63c0bea8f
astitva-22 <astitvashukla587@gmail.com>
2026-06-02 02:21:22 +0530

    Time: 15 ms (69.93%), Space: 26.3 MB (29.95%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+
+        for(auto x:strs){
+            string word=x;
+            sort(x.begin(),x.end());
+            mp[x].push_back(word);
+        }
+        vector<vector<string>> ans;
+        for(auto x : mp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
