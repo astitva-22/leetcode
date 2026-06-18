de3fb4609062ef15fa8919a5cb71b6ca7d99500b
astitva-22 <astitvashukla587@gmail.com>
2026-06-18 23:31:31 +0530

    Time: 4 ms (21.05%), Space: 40 MB (95.63%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,12 +4,9 @@ public:
         int n = nums.size();
         vector < int > ans(n,1);
         
-        // prefix
         for(int i = 1 ; i< n ; i++){
             ans[i]= ans[i-1]* nums[i-1];
         }
-
-        //  suffix
         int suffix = 1;
         for(int i = n-2 ; i >= 0 ; i--){
 
