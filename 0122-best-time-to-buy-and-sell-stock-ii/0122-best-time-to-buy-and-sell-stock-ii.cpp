e4b9566171af2451852cae60bfe2e105acdea623
astitva-22 <astitvashukla587@gmail.com>
2026-08-06 16:44:33 +0530

    Time: 8 ms (37.3%), Space: 24.8 MB (30.34%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    private:
+    long f(int i,int buy,vector<int> prices,vector<vector<int>> & dp){
+        int n=prices.size();
+        if(i==n) return 0;
+        long profit=0;
+        if(dp[i][buy]!=-1) return dp[i][buy];
+        if(buy){
+            profit =max(-prices[i]+f(i+1,0,prices,dp),0+f(i+1,1,prices,dp));
+        }
+        else profit =max(prices[i]+f(i+1,1,prices,dp),0+f(i+1,0,prices,dp));
+        return dp[i][buy]=profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+        // return f(0,1,prices,dp);
+        dp[n][0]=dp[n][1]=0;
+        int profit=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                if(buy){
+                    profit =max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                }
+                else profit =max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+                dp[i][buy]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+};
