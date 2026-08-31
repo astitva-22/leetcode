7ef4629793e9fe2ed21fd21e3f557b11bdeeb793
astitva-22 <astitvashukla587@gmail.com>
2026-08-31 20:50:36 +0530

    Time: 7 ms (33.57%), Space: 18.9 MB (36.23%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    private:
+    void buildadj(int numCourses, vector<vector<int>>& prerequisites,vector<vector<int>>& adj){
+        for(auto it: prerequisites){
+            int u=it[0];
+            int v=it[1];
+            adj[v].push_back(u);
+        }
+    }
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        buildadj(numCourses,prerequisites,adj);
+
+        vector<int> indeg(numCourses,0);
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            for(auto it:adj[i]){
+                indeg[it]++;
+            }
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        int cnt=0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
+            }
+        }
+        if(ans.size()!=numCourses) return {};
+        else return ans;
+    }
+};
